699c059aa9700d9ad73435a060b8c05a6e6472c1
Ram Pun <rampun@Rams-MacBook-Pro.local>
2023-03-22 23:54:59 +0800

    Home page modify - 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel CI CD</title>
-
-        <h1> This is a laravel CI CD Deployment </h1>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -32,6 +30,7 @@
                 </div>
             @endif
 
+            <h1> This is a laravel CI CD Deployment github and aws</h1>
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
